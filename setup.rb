9f24e756f9ffad452db536879eb8d695828cb122
Devan Goodwin <dgoodwin@redhat.com>
2014-04-07 12:27:50 -0300

    Eliminate bootstrap scripts and push repo setup to setup.rb.
    We now assume the user knows to register if necessary (on RHEL), and yum
    install ruby. After this we try to do everything in setup.rb.

--- setup.rb
+++ setup.rb
@@ -1,13 +1,40 @@
 #!/usr/bin/env ruby
 
+# TODO: automatically figure out the OS
+
+# TODO: devel user, sudo setup?
+# TODO: if run for devel user, make sure we run from correct directory
+
+# TODO: Would be nice to not require this:
+system('setenforce 0')
+
 if ARGV.include?('fedora19')
+
   system('yum -y localinstall http://fedorapeople.org/groups/katello/releases/yum/nightly/Fedora/19/x86_64/katello-repos-latest.rpm')
   system('yum -y localinstall http://yum.theforeman.org/nightly/f19/x86_64/foreman-release.rpm')
 
   # Facter parses the F19 fedora-release improperly due to the umlaut and apstrophe in the code name
   system('cp ./fedora-release /etc')
+
 elsif ARGV.include?('centos') || ARGV.include?('rhel')
 
+  # Clean out past runs if necessary:
+  system('rpm -e epel-release')
+  system('rpm -e foreman-release')
+  system('rpm -e katello-repos')
+  system('rm -f /etc/yum.repos.d/scl.repo')
+
+  if ARGV.include?('rhel')
+    # Setup RHEL specific repos
+    system('yum -y  --disablerepo="*" --enablerepo=rhel-6-server-rpms install yum-utils wget')
+    system('yum repolist') # TODO: necessary?
+    system('yum-config-manager --disable "*"')
+    system('yum-config-manager --enable rhel-6-server-rpms epel')
+    system('yum-config-manager --enable rhel-6-server-optional-rpms')
+    system('yum-config-manager --enable rhel-server-rhscl-6-rpms')
+  end
+
+  # NOTE: Using CentOS SCL even on RHEL to simplify subscription usage.
   if !File.directory?('/etc/yum.repos.d/scl.repo')
     system('cp ./scl.repo /etc/yum.repos.d/')
   end
@@ -33,5 +60,6 @@ if File.directory?('/vagrant/katello-installer')
     system("./bin/#{install_command} -v -d")
   end
 else
+  # TODO: get devel user options in here somehow:
   system("#{install_command} -v -d")
 end
