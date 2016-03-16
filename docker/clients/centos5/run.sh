0f48eaf42b4cf74075f84885f3a4faf04b3777c4
Eric D Helms <ericdhelms@gmail.com>
2016-03-16 16:31:49 -0400

    Update for move to dist based repo naming

--- docker/clients/centos5/run.sh
+++ docker/clients/centos5/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 echo "$FOREMAN_SERVER_IP $FOREMAN_SERVER_HOSTNAME" >> /etc/hosts
-wget https://fedorapeople.org/groups/katello/releases/yum/$KATELLO_VERSION/client/RHEL/5/x86_64/katello-client-repos-latest.rpm --no-check-certificate
+wget https://fedorapeople.org/groups/katello/releases/yum/$KATELLO_VERSION/client/el5/x86_64/katello-client-repos-latest.rpm --no-check-certificate
 yum -y localinstall katello-client-repos-*.rpm --nogpgcheck
 yum -y install katello-agent http://$FOREMAN_SERVER_HOSTNAME/pub/katello-ca-consumer-latest.noarch.rpm
 subscription-manager register --org $FOREMAN_ORGANIZATION --activationkey="$FOREMAN_ACTIVATION_KEY"
