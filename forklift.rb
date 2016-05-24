1464b121db4ba84268908a41acfcf2b497179f19
Eric D. Helms <ericdhelms@gmail.com>
2016-05-23 20:24:15 -0400

    Add command to turn directory into local-repo

--- forklift.rb
+++ forklift.rb
@@ -7,6 +7,7 @@ require File.join(File.dirname(__FILE__), 'lib/forklift')
 class MainCommand < Clamp::Command
 
   subcommand "koji-task", "Create a local repository from a Koji scratch build", Forklift::Command::KojiTaskCommand
+  subcommand "local-repo", "Turn a directory into a local repository for use on the system", Forklift::Command::LocalRepoCommand
 
 end
 
