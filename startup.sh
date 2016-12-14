a056e1e9c2c448a4d3972f7bd3209bf1aee75f0e
Eric D Helms <ericdhelms@gmail.com>
2016-12-14 07:38:52 -0500

    Changes to run successfully on OpenShift

--- startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo docker run -d --name "origin"         --privileged --pid=host --net=host         -v /:/rootfs:ro -v /var/run:/var/run:rw -v /sys:/sys -v /var/lib/docker:/var/lib/docker:rw         -v /var/lib/origin/openshift.local.volumes:/var/lib/origin/openshift.local.volumes         openshift/origin start
