018e5f8f97e81e56913a8f870aa0602a953217c0
Eric D Helms <ericdhelms@gmail.com>
2016-12-24 00:03:40 -0500

    Add Puppet provider to smart proxy

--- foreman-proxy/register.sh
+++ foreman-proxy/register.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -xe
 
+puppet agent -t
+
 curl -H "Content-Type: application/json" -X POST -d "{\"name\":\"foreman-proxy\",\"url\":\"http://${FOREMAN_PROXY_SERVICE_HOST}:8080\"}" -u admin:changeme http://$FOREMAN_SERVICE_HOST:8080/api/v2/smart_proxies
---
+++ foreman-proxy/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+puppet agent -t
+
+/usr/src/app/bin/smart-proxy
