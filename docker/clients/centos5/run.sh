20ec03e80739626ec68afd1959d80da60f8b64a3
Eric D. Helms <ericdhelms@gmail.com>
2017-06-29 20:17:03 -0400

    Remove unused docker directory

--- docker/clients/centos5/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-echo "$FOREMAN_SERVER_IP $FOREMAN_SERVER_HOSTNAME" >> /etc/hosts
-wget https://fedorapeople.org/groups/katello/releases/yum/$KATELLO_VERSION/client/el5/x86_64/katello-client-repos-latest.rpm --no-check-certificate
-yum -y localinstall katello-client-repos-*.rpm --nogpgcheck
-yum -y install katello-agent http://$FOREMAN_SERVER_HOSTNAME/pub/katello-ca-consumer-latest.noarch.rpm
-subscription-manager register --org $FOREMAN_ORGANIZATION --activationkey="$FOREMAN_ACTIVATION_KEY"
-service goferd stop
-exec goferd --foreground
--- docker/clients/fedora20/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-echo "$FOREMAN_SERVER_IP $FOREMAN_SERVER_HOSTNAME" >> /etc/hosts
-yum -y localinstall https://fedorapeople.org/groups/katello/releases/yum/$KATELLO_VERSION/client/f22/x86_64/katello-client-repos-latest.rpm
-yum -y install katello-agent http://$FOREMAN_SERVER_HOSTNAME/pub/katello-ca-consumer-latest.noarch.rpm
-subscription-manager register --org $FOREMAN_ORGANIZATION --activationkey="$FOREMAN_ACTIVATION_KEY"
-service goferd stop
-exec goferd --foreground
