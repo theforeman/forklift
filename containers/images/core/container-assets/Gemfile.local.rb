474ef4d3406d8125ad96f69527a387686cd94119
Ohad Levy <ohadlevy@gmail.com>
2018-05-06 17:41:01 +0300

    Build foreman container from source
    this is a Dockerfile based foreman container that is built from source
    
    I don't expect this to work fully yet, mostly out there for feedback.
    
    this image can be pulled via docker pull quay.io/ohadlevy/core

---
+++ containers/images/core/container-assets/Gemfile.local.rb
@@ -0,0 +1,10 @@
+gem 'puma-rails', platform: :ruby
+
+gem 'katello', git: 'https://github.com/katello/katello'
+gem 'foreman_ansible', git: 'https://github.com/theforeman/foreman_ansible'
+gem 'foreman_discovery', git: 'https://github.com/theforeman/foreman_discovery', branch: 'develop'
+gem 'foreman_bootdisk', git: 'https://github.com/theforeman/foreman_bootdisk'
+gem 'foreman_templates', git: 'https://github.com/theforeman/foreman_templates'
+gem 'foreman_openscap', git: 'https://github.com/theforeman/foreman_openscap'
+gem 'foreman_memcache', git: 'https://github.com/theforeman/foreman_memcache'
+gem 'foreman_remote_execution', git: 'https://github.com/theforeman/foreman_remote_execution'
