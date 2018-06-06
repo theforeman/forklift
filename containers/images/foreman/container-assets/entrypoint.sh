256329bbe409591f871f1beb96947e542e178bf0
Eric D. Helms <ericdhelms@gmail.com>
2018-06-05 20:49:33 -0400

    Bind to qpid queue to specific events for Foreman

--- containers/images/foreman/container-assets/entrypoint.sh
+++ containers/images/foreman/container-assets/entrypoint.sh
@@ -4,10 +4,10 @@ set -x
 
 /usr/bin/wait_on_postgres.py
 
+ansible-playbook /root/playbooks/create_qpid_queue.yml
+
 foreman-rake db:migrate
 echo $SEED_ADMIN_PASSWORD
 SEED_ADMIN_PASSWORD=$SEED_ADMIN_PASSWORD foreman-rake db:seed
-
-/usr/bin/create_qpid_queue.sh
 
 exec "$@"
