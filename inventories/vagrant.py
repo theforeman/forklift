ee7d5d5d79c56e16f2b79b2ef607263887629627
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-03-22 18:22:17 +0100

    Load ansible variables from vagrant into the inventory
    This rewrites the inventory to use Vagrants ansible variables as well.
    In the process we optimize the script as well to reduce calls to
    vagrant.

--- inventories/vagrant.py
+++ inventories/vagrant.py
@@ -3,10 +3,23 @@
 # https://github.com/ansible/ansible/blob/devel/plugins/inventory/vagrant.py
 import argparse
 import json
-import paramiko
+import os
 import subprocess
 import sys
-import os
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+import paramiko
+
+
+try:
+    DEVNULL = subprocess.DEVNULL
+except AttributeError:
+    DEVNULL = open(os.devnull, 'w')
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Vagrant inventory script")
@@ -16,44 +29,70 @@ def parse_args():
     return parser.parse_args()
 
 
-def list_running_hosts():
-    hosts = {}
-
-    with open(os.devnull, 'w') as FNULL:
-        try:
-            subprocess.check_call(["which","vagrant"], stdout=FNULL)
-        except subprocess.CalledProcessError as e:
-            return hosts
+def get_running_hosts():
+    try:
+        subprocess.check_call(["which", "vagrant"], stdout=DEVNULL)
+    except subprocess.CalledProcessError:
+        return
 
     cmd = "vagrant status --machine-readable"
     status = subprocess.check_output(cmd.split()).rstrip()
+
     for line in status.split('\n'):
         if len(line.split(',')) == 4:
             (_, host, key, value) = line.split(',')
         else:
-            (_, host, key, value, provider) = line.split(',')
+            (_, host, key, value, _) = line.split(',')
 
         if key == 'state' and value in ('active', 'running'):
-            hosts[host] = get_host_details(host)
-    return hosts
+            yield host
 
 
-def get_host_details(host):
-    cmd = "vagrant ssh-config {}".format(host)
-    with open(os.devnull, 'w') as FNULL:
-        try:
-            subprocess.check_call(cmd.split(), stdout=FNULL, stderr=FNULL)
-        except subprocess.CalledProcessError as e:
-            return {}
+def list_running_hosts():
+    hosts = list(get_running_hosts())
+    return dict(get_configs(hosts))
 
-    p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+
+def get_ssh_configs(hosts):
+    cmd = ['vagrant', 'ssh-config'] + hosts
+    try:
+        output = subprocess.check_output(cmd, stderr=DEVNULL)
+    except subprocess.CalledProcessError:
+        return {}
+
     config = paramiko.SSHConfig()
-    config.parse(p.stdout)
-    c = config.lookup(host)
-    return {'ansible_host': c['hostname'],
-            'ansible_port': c['port'],
-            'ansible_user': c['user'],
-            'ansible_ssh_private_key_file': c['identityfile'][0]}
+    config.parse(StringIO(output))
+    return config
+
+
+def get_host_ssh_config(config, host):
+    ssh = config.lookup(host)
+    return {'ansible_host': ssh['hostname'],
+            'ansible_port': ssh['port'],
+            'ansible_user': ssh['user'],
+            'ansible_ssh_private_key_file': ssh['identityfile'][0]}
+
+
+def get_variables(hosts):
+    cmd = [os.path.join(os.path.dirname(os.path.dirname(__file__)), 'bin', 'ansible-vars')] + hosts
+    try:
+        output = subprocess.check_output(cmd, stderr=DEVNULL)
+    except subprocess.CalledProcessError:
+        return {}
+
+    return json.loads(output)
+
+
+def get_configs(hosts):
+    ssh_configs = get_ssh_configs(hosts)
+    variables = get_variables(hosts)
+
+    for host in hosts:
+        details = {}
+        if host in variables:
+            details.update(variables[host])
+        details.update(get_host_ssh_config(ssh_configs, host))
+        yield host, details
 
 
 def main():
@@ -62,8 +101,8 @@ def main():
         hosts = list_running_hosts()
         json.dump(hosts, sys.stdout)
     elif args.host:
-        details = get_host_details(args.host)
-        json.dump(details, sys.stdout)
+        details = dict(get_configs([args.host]))
+        json.dump(details[args.host], sys.stdout)
 
 if __name__ == '__main__':
     main()
