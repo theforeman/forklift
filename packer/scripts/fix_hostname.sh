d742d9236e8de613b95da11ffa4bce8061f3e4b7
John Mitsch <jomitsch@redhat.com>
2019-12-13 13:54:12 -0500

    Increase resources for stable box and fix hostname env var issue
    This does two things:
    
    - Increases resources allocated for building the stable box. There are intermittent failures of building the stable box and I believe it is resource related. Note this is not the cores/RAM that will be on the box when spun up, that is set up by the box config (its 9GB of RAM currently), it is the amount packer uses when building the box.
    - Update HOSTNAME environment variable when changing the hostname before installing Katello. Since $HOSTNAME is already a set environment variable, we should use a custom environment variable to set this. This hasn't seemed to cause any issues yet, but I noticed an error when I was playing with custom certificates.
    
    This can be tested out using the instructions in `packer/README.md` with the branch checked out

--- packer/scripts/fix_hostname.sh
+++ packer/scripts/fix_hostname.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Configuring HOSTNAME: $HOSTNAME"
-hostnamectl set-hostname $HOSTNAME
+echo "Configuring HOSTNAME: $PACKER_HOSTNAME"
+hostnamectl set-hostname $PACKER_HOSTNAME
 SHORT_HOSTNAME=$(hostname -s)
-sed -i "1i127.0.0.1 $HOSTNAME $SHORT_HOSTNAME" /etc/hosts
-echo "HOSTNAME $HOSTNAME configured"
+sed -i "1i127.0.0.1 $PACKER_HOSTNAME $SHORT_HOSTNAME" /etc/hosts
+echo "HOSTNAME $PACKER_HOSTNAME configured"
