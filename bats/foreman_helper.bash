475203606074b70c3b238b51a9e7d4f281e7bd5e
Jeremy Lenz <jlenz@redhat.com>
2020-12-07 16:54:12 -0500

    Fixes #31453 - Add bats tests for CV import/export

--- bats/foreman_helper.bash
+++ bats/foreman_helper.bash
@@ -28,6 +28,12 @@ tKatelloVersion() {
   ) | cut -d. -f1-2
 }
 
+tSkipIfOlderThan318() {
+  if [[ tKatelloVersion < 3.18 ]]; then
+    skip "pulpcore import/export is not available on Katello versions older than 3.18"
+  fi
+}
+
 tSkipIfPulp3Only() {
   KATELLO_VERSION=$(tKatelloVersion)
   if [[ $KATELLO_VERSION == 4.* ]]; then
@@ -42,6 +48,35 @@ tIsPulp2() {
 tSkipIfPulp2() {
   if tIsPulp2; then
     skip "${1} is not available in scenarios with Pulp 2"
+}
+
+tSkipIfNoPulp2() {
+  if ! tIsPulp2; then
+    skip "${1} is not available in scenarios without Pulp 2"
+  fi
+}
+
+tIsPulpcore() {
+  tPackageExists python3-pulpcore
+}
+
+tSkipIfNoPulp3() {
+  if ! tIsPulpcore; then
+    skip "${1} is not available in scenarios without pulpcore"
+  fi
+}
+
+tSkipIfHammerBelow018() {
+  if tPackageExists tfm-rubygem-hammer_cli; then
+    RPM_PACKAGE=tfm-rubygem-hammer_cli
+  else
+    RPM_PACKAGE=rubygem-hammer_cli
+  fi
+  RPM_VERSION=$(rpm -q --queryformat '%{VERSION}' ${RPM_PACKAGE})
+
+  run rpmdev-vercmp $RPM_VERSION 0.18 > /dev/null
+  if [[ $status == 12 ]]; then
+    skip "Advanced content view tests are not available without hammer-cli >= 0.18"
   fi
 }
 
