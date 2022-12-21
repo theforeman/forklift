theforeman/forklift

commit acb404b0d936279fe49b15ffe20d1bb90bbed539

extend ansible bats test to also test role import and execution

bats/foreman_helper.bash
@@ -141,3 +141,14 @@ tForemanMaintainInstall() {
 tScenario() {
   basename $(readlink -f /etc/foreman-installer/scenarios.d/last_scenario.yaml) .yaml
 }
+
+tWaitForTask() {
+  local TASK_LABEL=$1
+  local next_wait_time=0
+  while hammer --no-headers task list --search="label=${TASK_LABEL} state=running" | grep "${TASK_LABEL}"; do
+    if [[ $next_wait_time -eq 12 ]]; then
+      break
+    fi
+    sleep $(( next_wait_time++ ))
+  done
+}

bats/fb-test-foreman-ansible.bats
@@ -2,7 +2,40 @@
 # vim: ft=sh:sw=2:et
 
 set -o pipefail
+load os_helper
+load foreman_helper
+
+setup() {
+  export ROLE_NAME=theforeman.batstest
+}
 
 @test "run 'uptime' via Ansible" {
   hammer job-invocation create --job-template 'Run Command - Ansible Default' --inputs 'command=uptime' --search-query "name = $HOSTNAME"
+}
+
+@test "import a role" {
+  # create a dummy role
+  mkdir -p "/etc/ansible/roles/${ROLE_NAME}/tasks"
+  echo "- command: uptime" > "/etc/ansible/roles/${ROLE_NAME}/tasks/main.yml"
+
+  # check if smart_proxy_ansible finds it and can import it
+  hammer ansible roles fetch --proxy-id 1 | grep "${ROLE_NAME}"
+  hammer ansible roles sync --proxy-id 1 --role-names "${ROLE_NAME}"
+
+  # sync creates a task, so let's wait for it to complete
+  tWaitForTask SyncRolesAndVariables
+
+  hammer ansible roles list | grep "${ROLE_NAME}"
+}
+
+@test "run a role" {
+  # assign the role to our system and run it
+  hammer host ansible-roles assign --name $HOSTNAME --ansible-roles "${ROLE_NAME}" | grep 'Ansible roles were assigned to the host'
+  hammer host ansible-roles play --name $HOSTNAME | grep 'Ansible roles are being played.'
+
+  # wait for the run to actually succeed
+  tWaitForTask Actions::RemoteExecution::RunHostsJob
+
+  # check if the callback reported things back to Foreman
+  hammer --output csv --no-headers config-report list --search "host=$HOSTNAME origin=Ansible" | grep "$HOSTNAME.*Ansible"
 }

commit 8e55cfb42ddcae80f18a0f70a92055a2411e6c26

correctly wait for tasks to be completed
1. check for `state != stopped` so that also planning and pending states
   are considered
2. use `$(… | wc -l) -ne 0` as not all tasks have their label in the
   output

Fixes: acb404b0d936279fe49b15ffe20d1bb90bbed539

bats/foreman_helper.bash
@@ -145,7 +145,7 @@ tScenario() {
 tWaitForTask() {
   local TASK_LABEL=$1
   local next_wait_time=0
-  while hammer --no-headers task list --search="label=${TASK_LABEL} state=running" | grep "${TASK_LABEL}"; do
+  while [[ $(hammer --no-headers task list --search="label=${TASK_LABEL} state != stopped" | wc -l) -ne 0 ]]; do
     if [[ $next_wait_time -eq 12 ]]; then
       break
     fi