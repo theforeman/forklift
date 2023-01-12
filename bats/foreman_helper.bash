theforeman/forklift

commit fff2579ae6927e446989f80f35d93f710a309962

fix shellcheck offenses

bats/foreman_helper.bash
@@ -44,7 +44,7 @@ tSkipIfOlderThan43() {
 
 tSkipIfNewerThan45() {
   KATELLO_VERSION=$(tKatelloVersion)
-  if [[ $KATELLO_VERSION > 4.5 ]]; then
+  if [[ $KATELLO_VERSION != 4.[0-4]* ]]; then
     skip "Skip if Katello is newer than 4.5"
   fi
 }

bats/fb-katello-client-global-registration.bats
@@ -24,7 +24,7 @@ load fixtures/content
 }
 
 @test "delete host if present" {
-  hammer host delete --name=$HOSTNAME || echo "Could not delete host"
+  hammer host delete --name="${HOSTNAME}" || echo "Could not delete host"
 }
 
 @test "register with global registration with activation key" {
@@ -33,7 +33,7 @@ load fixtures/content
   echo "${output}"
 
   organization_info=$(hammer --output json organization info --name "${ORGANIZATION}")
-  organization_id=$(echo $organization_info | ruby -e "require 'json'; puts JSON.load(ARGF.read)['Id']")
+  organization_id=$(echo "${organization_info}" | ruby -e "require 'json'; puts JSON.load(ARGF.read)['Id']")
 
   if tIsRHEL; then
     ignore_subman_errors=true
@@ -41,17 +41,17 @@ load fixtures/content
     ignore_subman_errors=false
   fi
 
-  curl_command="curl https://admin:changeme@$HOSTNAME/api/registration_commands -X POST -H 'Content-Type: application/json' -d '{\"activation_key\":\"${ACTIVATION_KEY}\",\"organization_id\":\"${organization_id}\",\"ignore_subman_errors\":\"${ignore_subman_errors}\"}'"
-  registration_json=$(eval $curl_command)
+  curl_command="curl https://admin:changeme@${HOSTNAME}/api/registration_commands -X POST -H 'Content-Type: application/json' -d '{\"activation_key\":\"${ACTIVATION_KEY}\",\"organization_id\":\"${organization_id}\",\"ignore_subman_errors\":\"${ignore_subman_errors}\"}'"
+  registration_json=$(eval "${curl_command}")
   echo "${registration_json}"
 
   registration_command=$(echo "${registration_json}" | ruby -e "require 'json'; puts JSON.load(ARGF.read).fetch('registration_command')")
-  eval $registration_command
+  eval "${registration_command}"
   tSubscribedProductOrSCA "${PRODUCT}"
 }
 
 @test "check content host is registered" {
-  hammer host info --name $HOSTNAME
+  hammer host info --name "${HOSTNAME}"
 }
 
 @test "enable content view repo" {
@@ -65,10 +65,10 @@ load fixtures/content
 
 @test "check available errata" {
   local next_wait_time=0
-  until hammer host errata list --host $HOSTNAME | grep 'RHEA-2012:0055'; do
+  until hammer host errata list --host "${HOSTNAME}" | grep 'RHEA-2012:0055'; do
     if [ $next_wait_time -eq 14 ]; then
       # make one last try, also makes the error nice
-      hammer host errata list --host $HOSTNAME | grep 'RHEA-2012:0055'
+      hammer host errata list --host "${HOSTNAME}" | grep 'RHEA-2012:0055'
     fi
     sleep $(( next_wait_time++ ))
   done
@@ -76,8 +76,8 @@ load fixtures/content
 
 @test "try fetching container content" {
   tPackageExists podman || tPackageInstall podman
-  podman login $HOSTNAME -u admin -p changeme
-  CONTAINER_PULL_LABEL=`echo "${ORGANIZATION_LABEL}-${PRODUCT_LABEL}-${CONTAINER_REPOSITORY_LABEL}"| tr '[:upper:]' '[:lower:]'`
+  podman login "${HOSTNAME}" -u admin -p changeme
+  CONTAINER_PULL_LABEL=$(echo "${ORGANIZATION_LABEL}-${PRODUCT_LABEL}-${CONTAINER_REPOSITORY_LABEL}"| tr '[:upper:]' '[:lower:]')
   podman pull "${HOSTNAME}/${CONTAINER_PULL_LABEL}"
 }
 

bats/fb-katello-client.bats
@@ -22,7 +22,7 @@ load fixtures/content
 }
 
 @test "delete host if present" {
-  hammer host delete --name=$HOSTNAME || echo "Could not delete host"
+  hammer host delete --name="${HOSTNAME}" || echo "Could not delete host"
 }
 
 @test "register subscription manager with username and password" {
@@ -52,7 +52,7 @@ load fixtures/content
 }
 
 @test "check content host is registered" {
-  hammer host info --name $HOSTNAME
+  hammer host info --name "${HOSTNAME}"
 }
 
 @test "enable content view repo" {
@@ -70,10 +70,10 @@ load fixtures/content
 
 @test "check available errata" {
   local next_wait_time=0
-  until hammer host errata list --host $HOSTNAME | grep 'RHEA-2012:0055'; do
+  until hammer host errata list --host "${HOSTNAME}" | grep 'RHEA-2012:0055'; do
     if [ $next_wait_time -eq 14 ]; then
       # make one last try, also makes the error nice
-      hammer host errata list --host $HOSTNAME | grep 'RHEA-2012:0055'
+      hammer host errata list --host "${HOSTNAME}" | grep 'RHEA-2012:0055'
     fi
     sleep $(( next_wait_time++ ))
   done
@@ -81,8 +81,8 @@ load fixtures/content
 
 @test "try fetching container content" {
   tPackageExists podman || tPackageInstall podman
-  podman login $HOSTNAME -u admin -p changeme
-  CONTAINER_PULL_LABEL=`echo "${ORGANIZATION_LABEL}-${PRODUCT_LABEL}-${CONTAINER_REPOSITORY_LABEL}"| tr '[:upper:]' '[:lower:]'`
+  podman login "${HOSTNAME}" -u admin -p changeme
+  CONTAINER_PULL_LABEL=$(echo "${ORGANIZATION_LABEL}-${PRODUCT_LABEL}-${CONTAINER_REPOSITORY_LABEL}"| tr '[:upper:]' '[:lower:]')
   podman pull "${HOSTNAME}/${CONTAINER_PULL_LABEL}"
 }
 
@@ -96,18 +96,18 @@ load fixtures/content
 
 @test "install package remotely (katello-agent)" {
   run yum -y remove gorilla
-  timeout 300 hammer host package install --host $HOSTNAME --packages gorilla
+  timeout 300 hammer host package install --host "${HOSTNAME}" --packages gorilla
   tPackageExists gorilla
 }
 
 @test "install errata remotely (katello-agent)" {
-  timeout 300 hammer host errata apply --errata-ids 'RHEA-2012:0055' --host $HOSTNAME
+  timeout 300 hammer host errata apply --errata-ids 'RHEA-2012:0055' --host "${HOSTNAME}"
   tPackageExists walrus-5.21
 }
 
 # it seems walrus lingers around making subsequent runs fail, so lets test package removal!
 @test "package remove (katello-agent)" {
-  timeout 300 hammer host package remove --host $HOSTNAME --packages walrus
+  timeout 300 hammer host package remove --host "${HOSTNAME}" --packages walrus
 }
 
 @test "clean up subscription-manager and gofer after content tests" {

bats/fb-test-foreman-ansible.bats
@@ -10,7 +10,7 @@ setup() {
 }
 
 @test "run 'uptime' via Ansible" {
-  hammer job-invocation create --job-template 'Run Command - Ansible Default' --inputs 'command=uptime' --search-query "name = $HOSTNAME"
+  hammer job-invocation create --job-template 'Run Command - Ansible Default' --inputs 'command=uptime' --search-query "name = ${HOSTNAME}"
 }
 
 @test "import a role" {
@@ -30,12 +30,12 @@ setup() {
 
 @test "run a role" {
   # assign the role to our system and run it
-  hammer host ansible-roles assign --name $HOSTNAME --ansible-roles "${ROLE_NAME}" | grep 'Ansible roles were assigned to the host'
-  hammer host ansible-roles play --name $HOSTNAME | grep 'Ansible roles are being played.'
+  hammer host ansible-roles assign --name "${HOSTNAME}" --ansible-roles "${ROLE_NAME}" | grep 'Ansible roles were assigned to the host'
+  hammer host ansible-roles play --name "${HOSTNAME}" | grep 'Ansible roles are being played.'
 
   # wait for the run to actually succeed
   tWaitForTask Actions::RemoteExecution::RunHostsJob
 
   # check if the callback reported things back to Foreman
-  hammer --output csv --no-headers config-report list --search "host=$HOSTNAME origin=Ansible" | grep "$HOSTNAME.*Ansible"
+  hammer --output csv --no-headers config-report list --search "host=${HOSTNAME} origin=Ansible" | grep "${HOSTNAME}.*Ansible"
 }

bats/fb-test-foreman-rex.bats
@@ -19,11 +19,11 @@ load foreman_helper
   # Force execution of the test in an own Organization
   # This ensures any *other* proxy with the REX capability is not used,
   # as that can lead to wrong results.
-  OLD_ORGS=$(hammer --output csv --no-headers --show-ids proxy info --name $SMART_PROXY_NAME --fields Organizations)
+  OLD_ORGS=$(hammer --output csv --no-headers --show-ids proxy info --name "${SMART_PROXY_NAME}" --fields Organizations)
   REX_ORG="Remote Execution Org $$"
   hammer organization create --name "${REX_ORG}"
-  hammer host update --name $SMART_PROXY_NAME --new-organization "${REX_ORG}"
-  hammer proxy update --name $SMART_PROXY_NAME --organizations "${OLD_ORGS},${REX_ORG}"
+  hammer host update --name "${SMART_PROXY_NAME}" --new-organization "${REX_ORG}"
+  hammer proxy update --name "${SMART_PROXY_NAME}" --organizations "${OLD_ORGS},${REX_ORG}"
 
   hammer job-invocation create --job-template "${job_template}" --inputs 'command=uptime' --search-query "name = $SMART_PROXY_NAME"
 }

bats/fb-test-foreman-templates.bats
@@ -7,7 +7,7 @@ load os_helper
 
 @test "export templates to a temp folder" {
   export_dir=$(runuser -u foreman -- mktemp --directory --tmpdir=/usr/share/foreman/tmp/ template-export.XXXXXXXXXX)
-  hammer export-templates --dirname / --repo ${export_dir}
+  hammer export-templates --dirname / --repo "${export_dir}"
 }
 
 @test "import templates from git" {

bats/fb-test-foreman.bats
@@ -21,7 +21,7 @@ load foreman_helper
 }
 
 @test "check web app is up" {
-  [ `curl -sk "https://localhost$URL_PREFIX/status" | ruby -e "require 'json'; puts JSON.load(ARGF.read)['status']"` == 'ok' ]
+  [ $(curl -sk "https://localhost$URL_PREFIX/status" | ruby -e "require 'json'; puts JSON.load(ARGF.read)['status']") = "ok" ]
 }
 
 @test "check hammer ping" {
@@ -42,5 +42,5 @@ load foreman_helper
 }
 
 @test "check smart proxy is registered" {
-  hammer proxy info --name=$(hostname -f)
+  hammer proxy info --name="$(hostname -f)"
 }

bats/fb-test-puppet.bats
@@ -21,7 +21,7 @@ fi
 }
 
 @test "check smart proxy is registered" {
-  hammer proxy info --name=$(hostname -f)
+  hammer proxy info --name="$(hostname -f)"
 }
 
 @test "assert puppet version" {
@@ -59,7 +59,7 @@ fi
 }
 
 @test "check host is registered" {
-  hammer host info --name $(hostname -f) | egrep "Last report:.*[[:alnum:]]+"
+  hammer host info --name "$(hostname -f)" | grep -E "Last report:.*[[:alnum:]]+"
 }
 
 # ENC / Puppet class apply tests
@@ -76,9 +76,9 @@ fi
 }
 
 @test "import motd puppet class" {
-  hammer proxy import-classes --name $(hostname -f)
+  hammer proxy import-classes --name "$(hostname -f)"
   count=$(hammer --csv puppet-class list --search 'name = motd' | wc -l)
-  [ $count -gt 1 ]
+  [ "${count}" -gt 1 ]
 }
 
 @test "Assign puppet-environment to default taxonomies" {
@@ -87,7 +87,7 @@ fi
 
 @test "assign puppet class to host" {
   id=$(hammer --csv puppet-class list --search 'name = motd' | tail -n1 | cut -d, -f1)
-  hammer host update --puppet-class-ids $id --name $(hostname -f)
+  hammer host update --puppet-class-ids "${id}" --name "$(hostname -f)"
 }
 
 @test "apply class with puppet agent" {

commit c7f07099c54c16e847a7b9fbabd3ae322c74c916

"newer than 4.5" should not include 4.5
Fixes: fff2579ae6927e446989f80f35d93f710a309962

bats/foreman_helper.bash
@@ -44,7 +44,7 @@ tSkipIfOlderThan43() {
 
 tSkipIfNewerThan45() {
   KATELLO_VERSION=$(tKatelloVersion)
-  if [[ $KATELLO_VERSION != 4.[0-4]* ]]; then
+  if [[ $KATELLO_VERSION != 4.[0-5]* ]]; then
     skip "Skip if Katello is newer than 4.5"
   fi
 }